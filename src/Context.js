bb2ba11fd657067e6b086dea91e17749ddd82eb2
Agnaldo Burgo Junior <agnaldo.junior@yapay.com.br>
2019-12-17 16:30:27 -0300

    add context pattern

---
+++ src/Context.js
@@ -0,0 +1,45 @@
+/* eslint-disable react/button-has-type */
+import React from 'react';
+
+import './styles.css';
+
+const themes = {
+  light: {
+    foreground: '#000000',
+    background: '#eeeeee',
+  },
+  dark: {
+    foreground: '#ffffff',
+    background: '#222222',
+  },
+};
+
+const ThemeContext = React.createContext(themes.dark);
+
+function App() {
+  return (
+    <ThemeContext.Provider value={themes.light}>
+      <Toolbar />
+    </ThemeContext.Provider>
+  );
+}
+
+function Toolbar() {
+  return (
+    <div>
+      <ThemedButton />
+    </div>
+  );
+}
+
+function ThemedButton() {
+  const theme = React.useContext(ThemeContext);
+
+  return (
+    <button style={{ background: theme.background, color: theme.foreground }}>
+      I am styled by theme context!
+    </button>
+  );
+}
+
+export default App;
